3737ce6027d07789e56584fc805f7a90a6ad4701
Andrei <andrei30.gherman@gmail.com>
2024-07-08 11:57:30 +0300

    Fix project

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import connectDB from "./config/dbConfig.js";
 import bodyParser from "body-parser";
+import artistRoutes from "./routes/artistRoutes.js";
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use('/api/artists', artistRoutes);
 
 connectDB();
 
